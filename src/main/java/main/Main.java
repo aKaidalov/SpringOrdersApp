f633dbf3ac119bf75e1100f9b1d5b9aae20e1828
Aleksandr Kaidalov <alkaid@ttu.ee>
2024-11-11 00:01:54 +0200

    hw09 - 4 | no sout in Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -19,14 +19,5 @@ public class Main {
                       Config.class, HsqlDataSource.class);
 
         OrderDao dao = ctx.getBean(OrderDao.class);
-
-//        System.out.println(dao);
-//
-//        dao.saveOrder(new Order("A123"));
-//        dao.saveOrder(new Order("B456"));
-//        dao.saveOrder(new Order("C789", List.of(new OrderRow("PC", 1, 1), new OrderRow("AC", 2, 2))));
-//
-//        System.out.println(dao.getAllOrders());
-
     }
 }
