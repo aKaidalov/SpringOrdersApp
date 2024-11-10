a3c96b4bfc7318001eca933a880ff0de914c00e6
Aleksandr Kaidalov <alkaid@ttu.ee>
2024-11-11 00:17:48 +0200

    hw09 - 6 | no sout in HsqlServer

--- src/main/java/main/HsqlServer.java
+++ src/main/java/main/HsqlServer.java
@@ -12,35 +12,6 @@ public class HsqlServer {
         Server server = new Server();
         server.setDatabasePath(0, "mem:db1;sql.syntax_pgs=true");
         server.setDatabaseName(0, "db1");
-        server.setLogWriter(new Logger(System.out));
         server.start();
     }
-
-    private static class Logger extends PrintWriter {
-        public Logger(OutputStream out) {
-            super(out);
-        }
-
-        @Override
-        public void print(String line) {
-            if (isSql(line)) {
-                System.out.println(line);
-            }
-        }
-
-        @Override
-        public void println() {
-        }
-
-        private boolean isSql(String line) {
-            for (String key : List.of("insert", "create",
-                    "select", "alter", "update")) {
-                if (line.toLowerCase().contains(key)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
 }
